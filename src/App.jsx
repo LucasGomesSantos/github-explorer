568319db536d5d8e0391253e00e3737fb66a9cc0
Lucas Gomes <31356490+LucasGomesSantos@users.noreply.github.com>
2021-03-10 19:19:48 -0300

    Typescript
    Inclusão de typescript no projeto e execução do metodo get nos diretorios

--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-import './styles/global.scss';
-
-export function App(){
-
-    return <h1>Ola mundo React JS</h1>
-}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {render} from 'react-dom';
+import './styles/global.scss';
+import { RepositoryList } from './components/RepositoryList'
+
+
+export function App(){
+
+    return  (
+     <RepositoryList/>
+
+    )
+}
